7308e4d4c4f515fa85d37a0f5ed80b34c85679ef
JackDevAU <jpr178@live.com.au>
2025-01-10 09:08:26 +1000

    quick fix for the rss

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        hostname: "assets.tina.io",
-      },
-    ],
-  },
-  async rewrites() {
-    return [
-      {
-        source: "/admin",
-        destination: "/admin/index.html",
-      },
-    ];
-  },
+	images: {
+		remotePatterns: [
+			{
+				hostname: "assets.tina.io",
+			},
+		],
+	},
+	async rewrites() {
+		return [
+			{
+				source: "/admin",
+				destination: "/admin/index.html",
+			},
+			{
+				// Rss was bad... ooops
+				source: "vercel-free-password-protection-nextjs",
+				destination: "/blob/vercel-free-password-protection-nextjs",
+			},
+		];
+	},
 };
 
 export default nextConfig;
